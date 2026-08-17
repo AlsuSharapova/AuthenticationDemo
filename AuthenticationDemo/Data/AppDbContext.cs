d0f9d94ccd3ec5fd764e0f65c68b5a0e1fd01596
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-17 19:13:56 +0400

    Create Database Using Identity Framework

---
+++ AuthenticationDemo/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AuthenticationDemo.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationDemo.Data {
+    public class AppDbContext : IdentityDbContext<Users> {
+        public AppDbContext(DbContextOptions options): base(options) {
+            
+        }
+    }
+}
---
+++ AuthenticationDemo/Models/Users.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationDemo.Models {
+    public class Users : IdentityUser{
+        public string FullName { get; set; }
+    }
+}
