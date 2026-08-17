74f20cd54aeaa25df28623d41f5fae55dd19bd35
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-17 20:18:56 +0400

    Add VerifyEmailViewModel and ChangePasswordViewModel

---
+++ AuthenticationDemo/ViewModels/VerifyEmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationDemo.ViewModels {
+    public class VerifyEmailViewModel {
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
