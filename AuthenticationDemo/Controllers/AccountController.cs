6c0aac0c075c895e03dabdcefd62b35fd1a722f0
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-19 17:07:40 +0400

    Add Register Action

--- AuthenticationDemo/Controllers/AccountController.cs
+++ AuthenticationDemo/Controllers/AccountController.cs
@@ -1,13 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthenticationDemo.Models;
+using AuthenticationDemo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationDemo.Controllers {
     public class AccountController : Controller {
+
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager) {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login() {
             return View();
         }
+
+        
         public IActionResult Register() {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model) {
+            if (ModelState.IsValid) {
+                Users users = new Users {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+
+                if (result.Succeeded) {
+                    return RedirectToAction("Login", "Account");
+                }
+                else {
+                    foreach(var error in result.Errors) {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
         public IActionResult VerifyEmail() {
             return View();
         }
