d417c28a732fdb44e4f57ee554c4a8981195046b
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-19 14:57:17 +0400

    Add AccountController and Login View

---
+++ AuthenticationDemo/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationDemo.Controllers {
+    public class AccountController : Controller {
+        public IActionResult Login() {
+            return View();
+        }
+    }
+}
