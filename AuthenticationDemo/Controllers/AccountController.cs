8ac5226efe56b79012fc9cdc9b64cda158a925b9
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-19 17:23:46 +0400

    Add VerifyEmail Action

--- AuthenticationDemo/Controllers/AccountController.cs
+++ AuthenticationDemo/Controllers/AccountController.cs
@@ -64,6 +64,22 @@ namespace AuthenticationDemo.Controllers {
         public IActionResult VerifyEmail() {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model) {
+            if (ModelState.IsValid) {
+                var user = await userManager.FindByNameAsync(model.Email);
+
+                if(user == null) {
+                    ModelState.AddModelError("", "Something is wrong.");
+                    return View(model);
+                }
+                else {
+                    return RedirectToAction("ChangePassword", "Account", new {username = user.UserName});
+                }
+            }
+            return View(model);
+        }
+
         public IActionResult ChangePassword() {
             return View();
         }
