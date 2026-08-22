860d938695219221340e4ec6968e5b28bcb2669a
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-22 20:08:55 +0400

    Add ConfirmEmailViewModel

---
+++ AuthenticationDemo/ViewModels/ConfirmEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationDemo.ViewModels {
+    public class ConfirmEmailViewModel {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter the confirmation code.")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "Code must be 6 characters long.")]
+        public string Code { get; set; }
+    }
+}
