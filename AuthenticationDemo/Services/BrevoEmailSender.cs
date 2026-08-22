e408261450942b2bf378722038b368a83a9d0b49
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-22 19:27:44 +0400

    Add BrevoEmailSender. Also deleted SendGrid Package and download SendWithBrevo package

---
+++ AuthenticationDemo/Services/BrevoEmailSender.cs
@@ -0,0 +1,27 @@
+﻿using SendWithBrevo;
+
+namespace AuthenticationDemo.Services {
+    public class BrevoEmailSender : IEmailSender {
+        private readonly IConfiguration configuration;
+
+        public BrevoEmailSender(IConfiguration configuration) {
+            this.configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage) {
+            var apiKey = configuration["Brevo:ApiKey"];
+            var fromEmail = configuration["Brevo:FromEmail"];
+            var fromName = configuration["Brevo:FromName"];
+
+            var client = new BrevoClient(apiKey);
+
+            await client.SendAsync(
+                new Sender(fromName, fromEmail),
+                new List<Recipient> { new Recipient(toEmail, toEmail) },
+                subject,
+                htmlMessage,
+                true
+            );
+        }
+    }
+}
