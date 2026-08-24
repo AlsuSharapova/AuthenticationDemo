90bdda87d7ae501d2adbfb0f6c5b27a0ea284bf1
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-24 13:55:19 +0400

    Fixed bug, forgot to register the service in DI.

--- AuthenticationDemo/Program.cs
+++ AuthenticationDemo/Program.cs
@@ -1,5 +1,6 @@
 using AuthenticationDemo.Data;
 using AuthenticationDemo.Models;
+using AuthenticationDemo.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<Users, IdentityRole>(options => {
@@ -24,11 +25,11 @@ builder.Services.AddIdentity<Users, IdentityRole>(options => {
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.AddTransient<IEmailSender, BrevoEmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
