197bb454600b582f4515c9d85dad66e3c5c357aa
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-25 17:07:41 +0400

    Add variable for token to Users

--- AuthenticationDemo/Models/Users.cs
+++ AuthenticationDemo/Models/Users.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
 namespace AuthenticationDemo.Models {
-    public class Users : IdentityUser{
+    public class Users : IdentityUser {
         public string FullName { get; set; }
         public string? EmailConfirmationCode { get; set; }
         public DateTime? EmailConfirmationCodeExpiry { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? PasswordResetTokenExpiry { get; set; }
     }
 }
