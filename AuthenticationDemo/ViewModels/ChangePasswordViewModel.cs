9b7ee96e17b00e010fd50cae2a1ac06656288f52
AlsuSharapova <alsusharapova1011@gmail.com>
2026-08-25 17:16:28 +0400

    Add Token to ChangePasswordViewModel

--- AuthenticationDemo/ViewModels/ChangePasswordViewModel.cs
+++ AuthenticationDemo/ViewModels/ChangePasswordViewModel.cs
@@ -7,6 +7,8 @@ namespace AuthenticationDemo.ViewModels {
         [EmailAddress]
         public string Email { get; set; }
 
+        public string Token { get; set; }
+
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
